9f036ad0b747e15efe4bc85beeffa11b918cc43a
Paweł Idzikowski <iddziku@gmail.com>
2020-03-18 13:01:05 +0100

    Further edition

---
+++ dragExample/Assets/Scripts/DragHandler.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class DragHandler : MonoBehaviour
+{
+    private GameObject _player;
+
+    private GameObject _draggableObject;
+
+    public static bool IsCarrying = false;
+
+    private void Start()
+    {
+        _player = GameObject.FindWithTag("Player");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (_draggableObject != null)
+            {
+                _draggableObject.GetComponent<BoxCollider>().enabled = false;
+
+                _draggableObject.transform.parent = _player.transform;
+
+                IsCarrying = true;
+            }
+        }
+        else
+        {
+            if (_draggableObject != null)
+            {
+                _draggableObject.GetComponent<BoxCollider>().enabled = true;
+
+                _draggableObject.transform.parent = null;
+
+                _draggableObject = null;
+            }
+
+            IsCarrying = false;
+        }
+    }
+
+    private void OnCollisionStay(Collision block)
+    {
+        if (block.gameObject.tag == "Draggable" & IsCarrying == false)
+        {
+            _draggableObject = block.gameObject;
+        }
+    }
+}
---
+++ dragExample/Assets/Scripts/MovementHandler.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+
+public class MovementHandler : MonoBehaviour
+{
+    private Animator _animator;
+
+    private Rigidbody _transform;
+
+    [SerializeField]
+    public float Speed;
+
+    private float _startingSpeed;
+
+    private float _carrySpeed;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+
+        _transform = GetComponent<Rigidbody>();
+
+        _startingSpeed = Speed;
+
+        // Carry speed 65% of basic speed
+        _carrySpeed = 0.65f * _startingSpeed;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // Stop walk animation
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) ||
+            Input.GetKeyUp(KeyCode.D))
+        {
+            _animator.SetBool("Move", false);
+        }
+
+        // Run walk animation
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) ||
+            Input.GetKey(KeyCode.D))
+        {
+            _animator.SetBool("Move", true);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (DragHandler.IsCarrying)
+        {
+            Speed = _carrySpeed;
+        }
+        else
+        {
+            Speed = _startingSpeed;
+        }
+
+
+        // Move forward
+        if (Input.GetKey(KeyCode.W))
+        {
+            _transform.AddForce(new Vector3(0, 0, 5) * Speed, ForceMode.VelocityChange);
+
+            if (DragHandler.IsCarrying == false)
+                _transform.rotation = Quaternion.LookRotation(Vector3.forward);
+        }
+
+        // Move left
+        if (Input.GetKey(KeyCode.A))
+        {
+            _transform.AddForce(new Vector3(-5, 0, 0) * Speed, ForceMode.VelocityChange);
+
+            if (DragHandler.IsCarrying == false)
+                _transform.rotation = Quaternion.LookRotation(Vector3.left);
+        }
+
+        // Move back
+        if (Input.GetKey(KeyCode.S))
+        {
+            _transform.AddForce(new Vector3(0, 0, -5) * Speed, ForceMode.VelocityChange);
+
+            if (DragHandler.IsCarrying == false)
+                _transform.rotation = Quaternion.LookRotation(Vector3.back);
+        }
+
+        // Move right
+        if (Input.GetKey(KeyCode.D))
+        {
+            _transform.AddForce(new Vector3(5, 0, 0) * Speed, ForceMode.VelocityChange);
+
+            if (DragHandler.IsCarrying == false)
+                _transform.rotation = Quaternion.LookRotation(Vector3.right);
+        }
+
+        // Apply little falling force
+        _transform.AddForce(new Vector3(0, -2, 0) * Speed, ForceMode.VelocityChange);
+    }
+}
