0c3845d9dc0810896209e8cf3171dd950248f6c6
Paweł Idzikowski <iddziku@gmail.com>
2020-03-18 15:09:56 +0100

    Update project

--- dragExample/Assets/Scripts/DragHandler.cs
+++ dragExample/Assets/Scripts/DragHandler.cs
@@ -6,24 +6,64 @@ public class DragHandler : MonoBehaviour
 
     public static bool IsCarrying = false;
 
+    private Vector2 startingPos;
+
+    private Vector2 currentPos;
+
+    private GameObject tmp;
+
     private void Update()
     {
         if (Input.GetKey(KeyCode.Space))
         {
             if (_draggableObject != null)
             {
-                _draggableObject.GetComponent<BoxCollider>().enabled = false;
+                _draggableObject.GetComponent<Collider>().enabled = false;
 
                 _draggableObject.transform.parent = gameObject.transform;
 
                 IsCarrying = true;
             }
         }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            if (_draggableObject != null)
+            {
+                tmp = _draggableObject;
+
+                _draggableObject.GetComponent<Rigidbody>().isKinematic = false;
+
+                var rigidbody = _draggableObject.GetComponent<Rigidbody>();
+                rigidbody.angularVelocity = Vector3.zero;
+
+                //if (gameObject.transform.rotation.y >= -90)
+                //{
+                //    rigidbody.velocity += Vector3.up * Physics.gravity.y * Time.deltaTime;
+                //}
+                //else if (gameObject.transform.rotation.y == 180)
+                //{
+                //    _draggableObject.GetComponent<Rigidbody>().AddForce(-transform.forward, ForceMode.VelocityChange);
+                //}
+                //else if (gameObject.transform.rotation.y == 0)
+                //{
+                //    _draggableObject.GetComponent<Rigidbody>().AddForce(transform.forward, ForceMode.VelocityChange);
+                //}
+                //else if (gameObject.transform.rotation.y >= 90)
+                //{
+                //    _draggableObject.GetComponent<Rigidbody>().AddForce(transform.right, ForceMode.VelocityChange);
+                //}
+
+                // rigidbody.velocity += Vector3.down * Physics.gravity.y * 555 * Time.deltaTime;
+
+                _draggableObject.GetComponent<Rigidbody>().AddForce(transform.forward, ForceMode.Impulse);
+                Invoke("ReturnIsKinematic", 5f);
+            }
+        }
         else
         {
             if (_draggableObject != null)
             {
-                _draggableObject.GetComponent<BoxCollider>().enabled = true;
+                _draggableObject.GetComponent<Collider>().enabled = true;
 
                 _draggableObject.transform.parent = null;
 
@@ -39,6 +79,13 @@ public class DragHandler : MonoBehaviour
         if (block.gameObject.CompareTag("Draggable") & IsCarrying == false)
         {
             _draggableObject = block.gameObject;
+
+            startingPos = block.gameObject.transform.position;
         }
+    }
+    
+    private void ReturnIsKinematic()
+    {
+        tmp.GetComponent<Rigidbody>().isKinematic = true;
     }
 }
